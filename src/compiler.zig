a377e13dbd83f7b5718b25ff42b4ee9332867060
Aaron Patterson <tenderlove@ruby-lang.org>
2024-11-05 11:46:41 -0800

    fix register allocation

--- src/compiler.zig
+++ src/compiler.zig
@@ -9,17 +9,48 @@ const Register = struct {
     number: u32,
 };
 
-const Instruction = struct {
-    const Name = enum {
-        move,
-        loadi,
-        add
-    };
+const InstructionName = enum {
+    move,
+    loadi,
+    add
+};
 
-    insn: Name,
-    out: Register,
-    in1: Register,
-    in2: Register,
+const Instruction = union(InstructionName) {
+    move: struct {
+        out: Register,
+        in1: Register,
+    },
+
+    loadi: struct {
+        out: Register,
+        val: u64,
+    },
+
+    add: struct {
+        out: Register,
+        in1: Register,
+        in2: Register,
+    },
+
+    fn printNode(self: ?*InstructionList.Node) void {
+        if (self == null) { return; }
+
+        const node = self.?.data;
+        switch(node) {
+            .move => |v| {
+                std.debug.print("move {d} {d}\n", .{ v.out.number, v.in1.number });
+            },
+            .loadi => |v| {
+                std.debug.print("loadi {d} {d}\n", .{ v.out.number, v.val });
+            },
+            .add => |v| {
+                std.debug.print("add   {d} {d} {d}\n", .{ v.out.number, v.in1.number, v.in2.number });
+            },
+        }
+
+        printNode(self.?.next);
+
+    }
 };
 
 const InstructionList = std.DoublyLinkedList(Instruction);
@@ -38,6 +69,11 @@ pub const Compiler = struct {
     allocator: std.mem.Allocator,
 
     pub fn compile(cc: *Compiler, node: [*c]const c.pm_node_t) error{NotImplementedError, OutOfMemory}!Register {
+        const reg = compileNode(cc, node);
+        return reg;
+    }
+
+    pub fn compileNode(cc: *Compiler, node: [*c]const c.pm_node_t) error{NotImplementedError, OutOfMemory}!Register {
         return switch (node.*.type) {
             c.PM_CALL_NODE => try cc.compileCallNode(@ptrCast(node)),
             c.PM_INTEGER_NODE => try cc.compileIntegerNode(@ptrCast(node)),
@@ -64,25 +100,27 @@ pub const Compiler = struct {
         if (node.*.receiver == null) {
             return error.NotImplementedError;
         } else {
-            const recv_op = try cc.compile(node.*.receiver);
+            const recv_op = try cc.compileNode(node.*.receiver);
             const arg_size = node.*.arguments.*.arguments.size;
             const args = node.*.arguments.*.arguments.nodes[0..arg_size];
 
             if (std.mem.eql(u8, method_name, "+") and arg_size == 1) {
                 const insn = try cc.allocator.create(InstructionList.Node);
+                const in2 = try cc.compileNode(args[0]);
 
                 insn.* = InstructionList.Node {
-                    .data = .{
-                        .insn = Instruction.Name.add,
-                        .out = try cc.newRegister(),
-                        .in1 = recv_op,
-                        .in2 = try cc.compile(args[0]),
+                    .data = Instruction {
+                        .add = .{
+                            .out = try cc.newRegister(),
+                            .in1 = recv_op,
+                            .in2 = in2,
+                        }
                     }
                 };
 
-                cc.scopes.first.?.data.insns.prepend(insn);
+                cc.scopes.first.?.data.insns.append(insn);
 
-                return insn.data.out;
+                return insn.data.add.out;
             } else {
                 return error.NotImplementedError;
             }
@@ -104,13 +142,33 @@ pub const Compiler = struct {
         };
         cc.scopes.prepend(scope);
 
-        return try cc.compile(node.*.body);
+        const reg = try cc.compileNode(node.*.body);
+
+        Instruction.printNode(scope.data.insns.first);
+
+        return reg;
     }
 
     fn compileIntegerNode(cc: *Compiler, node: *const c.pm_integer_node_t) !Register {
-        const outreg = cc.newRegister();
-        _ = node;
-        return outreg;
+        if (node.*.value.values == null) {
+            std.debug.print("integer {d}\n", .{ node.*.value.value });
+            const insn = try cc.allocator.create(InstructionList.Node);
+
+            insn.* = InstructionList.Node {
+                .data = Instruction {
+                    .loadi = .{
+                        .out = try cc.newRegister(),
+                        .val = node.*.value.value,
+                    }
+                }
+            };
+
+            cc.scopes.first.?.data.insns.append(insn);
+
+            return insn.data.loadi.out;
+        } else {
+            return error.NotImplementedError;
+        }
     }
 
     fn compileStatementsNode(cc: *Compiler, node: [*c]const c.pm_statements_node_t) !Register {
@@ -121,10 +179,9 @@ pub const Compiler = struct {
                 _ = item;
                 _ = i;
                 return error.NotImplementedError;
-                //cc.compile(
             }
             std.debug.print("len {d}\n", .{body.*.size});
-            return try cc.compile(body.*.nodes[body.*.size - 1]);
+            return try cc.compileNode(body.*.nodes[body.*.size - 1]);
         } else {
             return error.NotImplementedError;
         }
