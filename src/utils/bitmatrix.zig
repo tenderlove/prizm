2fc143e2b46169d6f36eb4e576ec54992a3555d4
Aaron Patterson <tenderlove@ruby-lang.org>
2024-12-04 13:23:47 -0800

    Probably doesn't matter, but we can specify a matrix size
    I just wanted to try abstracting the matrix away from the underlying
    buffer byte type.

--- src/utils/bitmatrix.zig
+++ src/utils/bitmatrix.zig
@@ -1,47 +1,69 @@
 const std = @import("std");
 
-pub const BitMatrix = struct {
-    x: usize,
-    y: usize,
-    row_len: usize,
-    buffer: []u8,
+pub fn BitMatrixSize(comptime T: type) type {
+    return struct {
+        const Self = @This();
 
-    pub fn init(mem: std.mem.Allocator, x: usize, y: usize) !*BitMatrix {
-        const bm = try mem.create(BitMatrix);
+        x: usize,
+        y: usize,
+        row_len: usize,
+        buffer: []T,
 
-        // x and y are in number of bits, so lets convert to bytes
-        // since the buffer will be u8.
-        const roundx = (x + 7) & ~@as(usize, 7);
-        const roundy = (y + 7) & ~@as(usize, 7);
-        const rowlen = roundx / 8;
+        pub fn init(mem: std.mem.Allocator, x: usize, y: usize) !*Self {
+            const bm = try mem.create(Self);
 
-        const buff_size = rowlen + (roundy / 8);
+            const coef = @typeInfo(T).int.bits;
 
-        const buff = try mem.alloc(u8, buff_size);
-        @memset(buff, 0);
-        bm.* = .{ .x = x, .y = y, .row_len = rowlen, .buffer = buff };
-        return bm;
-    }
+            // x and y are in number of bits, so lets convert to bytes
+            // since the buffer will be u8.
+            const roundx = (x + (coef - 1)) & ~@as(usize, (coef - 1));
+            const roundy = (y + (coef - 1)) & ~@as(usize, (coef - 1));
+            const rowlen = roundx / coef;
 
-    pub fn set(self: *const BitMatrix, x: usize, y: usize) void {
-        const row = x * self.row_len;
-        const column = y / 8;
-        const bit = @as(u8, 1) << @as(u3, @intCast(@mod(y, 8)));
-        self.buffer[row + column] |= bit;
-    }
+            const buff_size = rowlen + (roundy / coef);
 
-    pub fn isSet(self: *const BitMatrix, x: usize, y: usize) bool {
-        const row = x * self.row_len;
-        const column = y / 8;
-        const bit = @as(u8, 1) << @as(u3, @intCast(@mod(y, 8)));
-        return (self.buffer[row + column] & bit) == bit;
-    }
+            const buff = try mem.alloc(T, buff_size);
+            @memset(buff, 0);
+            bm.* = .{ .x = x, .y = y, .row_len = rowlen, .buffer = buff };
+            return bm;
+        }
 
-    pub fn deinit(self: *const BitMatrix, mem: std.mem.Allocator) void {
-        mem.free(self.buffer);
-        mem.destroy(self);
-    }
-};
+        pub fn set(self: *const Self, x: usize, y: usize) void {
+            const row = x * self.row_len;
+            const bits = @typeInfo(T).int.bits;
+            const column = y / bits;
+            const bit = @as(T, 1) << @intCast(@mod(y, bits));
+            self.buffer[row + column] |= bit;
+        }
+
+        pub fn isSet(self: *const Self, x: usize, y: usize) bool {
+            const row = x * self.row_len;
+            const bits = @typeInfo(T).int.bits;
+            const column = y / bits;
+            const bit = @as(T, 1) << @intCast(@mod(y, bits));
+            return (self.buffer[row + column] & bit) == bit;
+        }
+
+        pub fn deinit(self: *const Self, mem: std.mem.Allocator) void {
+            mem.free(self.buffer);
+            mem.destroy(self);
+        }
+    };
+}
+
+pub const BitMatrix = BitMatrixSize(u8);
+
+test "bit64" {
+    const alloc = std.testing.allocator;
+
+    const bm64 = try BitMatrixSize(u64).init(alloc, 64, 64);
+    defer bm64.deinit(alloc);
+    try std.testing.expectEqual(2, bm64.buffer.len);
+
+    const bm8 = try BitMatrixSize(u8).init(alloc, 64, 64);
+    defer bm8.deinit(alloc);
+    try std.testing.expectEqual(16, bm8.buffer.len);
+}
 
 test "can allocate" {
     const alloc = std.testing.allocator;
