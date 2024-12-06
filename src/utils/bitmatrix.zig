4fea3d2e8e9cd480269463db4b70d8659d703e28
Aaron Patterson <tenderlove@ruby-lang.org>
2024-12-05 16:56:03 -0800

    fixing bug in bitmatrix

--- src/utils/bitmatrix.zig
+++ src/utils/bitmatrix.zig
@@ -10,22 +10,24 @@ pub fn BitMatrixSize(comptime T: type) type {
         row_len: usize,
         buffer: []T,
 
-        pub fn init(mem: std.mem.Allocator, x: usize, y: usize) !*Self {
+        pub fn init(mem: std.mem.Allocator, rows: usize, y: usize) !*Self {
             const bm = try mem.create(Self);
 
-            const coef = @typeInfo(T).int.bits;
+            const atom_bits = @typeInfo(T).int.bits;
 
-            // x and y are in number of bits, so lets convert to bytes
-            // since the buffer will be u8.
-            const roundx = (x + (coef - 1)) & ~@as(usize, (coef - 1));
-            const roundy = (y + (coef - 1)) & ~@as(usize, (coef - 1));
-            const rowlen = roundx / coef;
+            // Round row bits up to the nearest multiple of our atom bits
+            // For example, if atom is a u64, then we round 32 up to 64,
+            // or 110 to 128.
+            const roundy = (y + (atom_bits - 1)) & ~@as(usize, (atom_bits - 1));
 
-            const buff_size = rowlen + (roundy / coef);
+            // Calculate the number of atoms each row needs.
+            const atoms_per_row = roundy / atom_bits;
+
+            const buff_size = rows * atoms_per_row;
 
             const buff = try mem.alloc(T, buff_size);
             @memset(buff, 0);
-            bm.* = .{ .rows = x, .columns = y, .row_len = rowlen, .buffer = buff };
+            bm.* = .{ .rows = rows, .columns = y, .row_len = atoms_per_row, .buffer = buff };
             return bm;
         }
 
@@ -48,7 +50,7 @@ pub fn BitMatrixSize(comptime T: type) type {
         pub fn getRow(self: Self, x: usize) bitmap.BitMapSized(T) {
             const row = x * self.row_len;
             const slice = self.buffer[row..(row + self.row_len)];
-            return bitmap.BitMapSized(T).initShared(self.rows, slice);
+            return bitmap.BitMapSized(T).initShared(self.columns, slice);
         }
 
         pub fn deinit(self: *const Self, mem: std.mem.Allocator) void {
@@ -65,11 +67,13 @@ test "bit64" {
 
     const bm64 = try BitMatrixSize(u64).init(alloc, 64, 64);
     defer bm64.deinit(alloc);
-    try std.testing.expectEqual(2, bm64.buffer.len);
+
+    // Number of rows * Atoms per row
+    try std.testing.expectEqual(64, bm64.buffer.len);
 
     const bm8 = try BitMatrixSize(u8).init(alloc, 64, 64);
     defer bm8.deinit(alloc);
-    try std.testing.expectEqual(16, bm8.buffer.len);
+    try std.testing.expectEqual(64 * (64 / 8), bm8.buffer.len);
 }
 
 test "can allocate" {
@@ -88,7 +92,7 @@ test "buffer is rounded up" {
     const matrix = try BitMatrix.init(alloc, 8, 7);
     defer matrix.deinit(alloc);
 
-    try std.testing.expectEqual((8 / 8) + (8 / 8), matrix.buffer.len);
+    try std.testing.expectEqual(8, matrix.buffer.len);
 }
 
 test "set x y" {
@@ -127,12 +131,26 @@ test "get row as bitmap" {
     matrix.set(1, 5);
 
     const bm = matrix.getRow(0);
-    try std.testing.expectEqual(8, bm.getBits());
+    try std.testing.expectEqual(2048, bm.getBits());
     try std.testing.expect(bm.isSet(0));
     try std.testing.expect(bm.isSet(1));
 
     const bm2 = matrix.getRow(1);
-    try std.testing.expectEqual(8, bm2.getBits());
+    try std.testing.expectEqual(2048, bm2.getBits());
     try std.testing.expect(bm2.isSet(4));
     try std.testing.expect(bm2.isSet(5));
+}
+
+test "wtf" {
+    const alloc = std.testing.allocator;
+
+    // 3 rows, 1 column
+    const matrix = try BitMatrix.init(alloc, 3, 1);
+    defer matrix.deinit(alloc);
+
+    // 3 rows * atoms/row (u8)
+    try std.testing.expectEqual(3, matrix.buffer.len);
+
+    matrix.set(0, 0);
+    matrix.set(2, 0);
 }
